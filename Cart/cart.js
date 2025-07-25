6579716276692cade3a08524e920ade5bbf31c7f
voidcht <chtlansakara@gmail.com>
2025-07-25 23:50:29 +0530

    Maximum and minimum for quantity update are set.

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,14 +1,27 @@
 let cartQuantity = 0;
 
 function addToCart(quantity){
+
+  // setting the maximum with if
+  if((cartQuantity+quantity) <= 10){
   cartQuantity+=quantity;
   console.log(`Cart quantity: ${cartQuantity}`);
   showCartQuantity();
+  } else{
+    //a pop up for cart full
+    alert('Can\'t update! The cart is full!');
+  }
 }
 function removeFromCart(quantity){
+  //setting the minimum with if
+  if((cartQuantity-quantity)>=0){
   cartQuantity-=quantity;
   console.log(`Cart quantity: ${cartQuantity}`);
   showCartQuantity();
+  }else{
+    //a pop up for error for minus quantity
+    alert('Can\'t update! Not enough items in the cart!');
+  }
 }
 
 function showCartQuantity(){
