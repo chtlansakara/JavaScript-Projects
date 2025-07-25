f2118bb4acdc0486e15447a287df9a15ea672459
voidcht <chtlansakara@gmail.com>
2025-07-25 23:34:03 +0530

    Swiched to functions

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,1 +1,25 @@
 let cartQuantity = 0;
+
+function addToCart(quantity){
+  cartQuantity+=quantity;
+  console.log(`Cart quantity: ${cartQuantity}`);
+  showCartQuantity();
+}
+function removeFromCart(quantity){
+  cartQuantity-=quantity;
+  console.log(`Cart quantity: ${cartQuantity}`);
+  showCartQuantity();
+}
+
+function showCartQuantity(){
+  const displayElement = document.querySelector('.display-box');
+  displayElement.innerHTML = `Cart quantity: ${cartQuantity}`;
+}
+
+function resetCart(){
+  cartQuantity = 0;
+
+  console.log('Cart was reset!');
+  console.log(`Cart quantity: ${cartQuantity}`);
+  showCartQuantity();
+}
