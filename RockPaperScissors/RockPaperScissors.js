51033fa7d822819cf4ac6caef2f577e387de91c0
voidcht <chtlansakara@gmail.com>
2025-07-26 15:01:20 +0530

    RockPaperScissors- Code cleaned and new function to show results added.

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -1,92 +1,140 @@
 
-
-      // adding event listners to the button 'click' events
-      rockButtonElement = document.querySelector('.js-rock-button');
-      paperButtonElement = document.querySelector('.js-paper-button');
-      scissorsButtonElement = document.querySelector('.js-scissors-button');
-
-      rockButtonElement.addEventListener('click', () => {
-        console.log('rock');
-        playGame('rock');
-      });
-      paperButtonElement.addEventListener('click', () => {
-        console.log('paper');
-        playGame('paper');
-      });
-      scissorsButtonElement.addEventListener('click', () => {
-        console.log('scissors');
-        playGame('scissors');
-      });
+// keeping the score in an objects
+const score = {
+  wins : 0,
+  loses: 0,
+  ties: 0
+};
 
 
-      //function to run - when an  option is selected by the user
+
+
+    //function 
+      // to run - when an  option is selected by the user
+
       function playGame(userMove) {
+
         //the computer's move
         const computerMove = selectComputerMove();
         let result = '';
 
         //result
+        //  when 'rock' is chosen by the user
         if (userMove === 'rock') {
           if (computerMove === 'rock') {
-            result = 'Tie';
+            result = 'tied';
+           
           } else if (computerMove === 'paper') {
-            result = 'You lost';
+            result = 'lost';
+            
           } else if (computerMove === 'scissors') {
-            result = 'You won';
+            result = 'won';
           }
         }
+        //  when 'paper' is chosen by the user
         if (userMove === 'paper') {
           if (computerMove === 'rock') {
-            result = 'You won';
+            result = 'won';
           } else if (computerMove === 'paper') {
-            result = 'Tie';
+            result = 'tied';
           } else if (computerMove === 'scissors') {
-            result = 'You lost';
+            result = 'lost';
           }
         }
+        //  when 'scissors' is chosen by the user
         if (userMove === 'scissors') {
           if (computerMove === 'rock') {
-            result = 'You lost';
+            result = 'lost';
           } else if (computerMove === 'paper') {
-            result = 'You won';
+            result = 'won';
           } else if (computerMove === 'scissors') {
-            result = 'Tie';
+            result = 'tied';
           }
         }
 
-        alert(`You picked ${userMove}, Computer picked ${computerMove}, ${result}.`);
+        //update score
+        updateScore(result);
+
+        // show results
+        showResults(userMove, computerMove, result);
+      }
+
+
+
+    //function
+      //-to display result
+
+      function showResults(userMove, computerMove, result){
+        
+        //adding 'you' for 'won' and 'lose' & for 'tie', capitalize first letter
+        if(result === 'won' || result === 'lost'){
+          result = `You ${result}`;
+        }if( result === 'tied'){
+          result = 'Tied';
+        }
+
+        alert(`You picked ${userMove},\nComputer picked ${computerMove},\n${result}!`);
 
 
       }
 
 
 
-      //function to select automatic computer move
+    //function 
+      // -to select computer move
+
       function selectComputerMove() {
+
         let computerMove = '';
 
         //using random number to select an option ( 0 <= random  < 1)
         const rand = Math.random();
-        console.log(rand);
+        // console.log(rand);
 
         //choosing one of 3 option
         if (rand >= 0 && rand <= 1 / 3) {
           computerMove = 'rock';
-          console.log('computer chose rock');
+          // console.log('computer chose rock');
         } else if (rand > 1 / 3 && rand <= 2 / 3) {
           computerMove = 'paper';
-          console.log('computer chose paper');
+          // console.log('computer chose paper');
 
         } else if (rand > 2 / 3 && rand < 1) {
           computerMove = 'scissors';
-          console.log('computer chose scissors');
+          // console.log('computer chose scissors');
 
         } else {
-          console.log('No computer move found!');
+          // console.log('No computer move found!');
         }
 
         return computerMove;
 
       }
 
-  
+    //function
+     //-to update the score
+     function updateScore(result){
+
+      // if( result = '')
+
+     }  
+
+
+
+
+
+    //ADDITIONAL: 
+      // adding event listners to the button 'click' events
+      rockButtonElement = document.querySelector('.js-rock-button');
+      paperButtonElement = document.querySelector('.js-paper-button');
+      scissorsButtonElement = document.querySelector('.js-scissors-button');
+
+      rockButtonElement.addEventListener('click', () => {
+        playGame('rock');
+      });
+      paperButtonElement.addEventListener('click', () => {
+        playGame('paper');
+      });
+      scissorsButtonElement.addEventListener('click', () => {
+        playGame('scissors');
+      });
